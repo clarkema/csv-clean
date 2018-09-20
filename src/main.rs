58f4ab9c656bcfa4f7501add93eb530058d6f8e8
Mike Clarke <mike@lambdafunctions.com>
2018-09-20 14:31:45 +0100

    Read either from a specified file or STDIN

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 #[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate clap;
 extern crate csv;
 
 use std::io::{self, BufWriter, Write};
 use std::process;
+use std::fs::File;
 
 const BUFSIZE: usize = 8192;
 
 type CleanedRecord = Vec<String>;
 
 fn main() {
+    let matches = clap_app!(csv_clean =>
+        (version: "pre")
+        (author: "Mike Clarke <mike@lambdafunctions.com>")
+        (@arg input: "Input file.  Omit or use '-' for STDIN.")
+    ).get_matches();
+
+    let input_file = matches.value_of("input").unwrap_or("-");
+
     let mut last_line = CleanedRecord::new();
+
+    let reader: Box<io::Read> = if input_file == "-" {
+        Box::new(io::stdin())
+    }
+    else {
+        Box::new(File::open(input_file).unwrap())
+    };
 
     let mut rdr = csv::ReaderBuilder::new()
         .trim(csv::Trim::All)
-        .from_reader(io::stdin());
+        .from_reader(reader);
 
     if let Ok(headers) = rdr.headers() {
         for _ in headers.iter() {
