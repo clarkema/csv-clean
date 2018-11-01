24f0428e3ad029de3d25e05d96d4d04b52b975ea
Mike Clarke <mike@lambdafunctions.com>
2018-11-01 17:10:16 +0000

    Add 'cascade' option to control whether to default to cell above

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,11 @@ fn main() {
         (version: "pre")
         (author: "Mike Clarke <mike@lambdafunctions.com>")
         (@arg input: "Input file.  Omit or use '-' for STDIN.")
+        (@arg cascade: --cascade "Fill empty cells with values from row above")
     ).get_matches();
 
     let input_file = matches.value_of("input").unwrap_or("-");
+    let cascade_p = matches.is_present("cascade");
 
     let mut last_line = CleanedRecord::new();
 
@@ -45,7 +47,7 @@ fn main() {
     for result in rdr.records() {
         match result {
             Ok(record) => {
-                process_line(record, &mut last_line);
+                process_line(record, &mut last_line, cascade_p);
                 writeln!(writer, "{}", json!(last_line)).unwrap();
             }
             Err(err) => {
@@ -56,10 +58,10 @@ fn main() {
     }
 }
 
-fn process_line(line: csv::StringRecord, last_line: &mut CleanedRecord) {
+fn process_line(line: csv::StringRecord, last_line: &mut CleanedRecord, cascade_p: bool) {
     for i in 0..(line.len()) {
         if let Some(field) = line.get(i) {
-            if field.len() > 0 {
+            if field.len() > 0 || ! cascade_p {
                 last_line[i].clear();
                 last_line[i].push_str(field);
             }
