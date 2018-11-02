0e46aadce4f6eae88d6025030b6c04bddcc5f543
Mike Clarke <mike@lambdafunctions.com>
2018-11-02 16:27:18 +0000

    Add basic date conversion

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,35 @@
 extern crate serde_json;
 #[macro_use]
 extern crate clap;
+extern crate chrono;
 extern crate csv;
 
+use clap::App;
 use std::io::{self, BufWriter, Write};
 use std::process;
 use std::fs::File;
+use chrono::prelude::*;
 
 const BUFSIZE: usize = 8192;
 
 type CleanedRecord = Vec<String>;
 
 fn main() {
-    let matches = clap_app!(csv_clean =>
-        (version: "pre")
-        (author: "Mike Clarke <mike@lambdafunctions.com>")
-        (@arg input: "Input file.  Omit or use '-' for STDIN.")
-        (@arg cascade: --cascade "Fill empty cells with values from row above")
-    ).get_matches();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
     let input_file = matches.value_of("input").unwrap_or("-");
     let cascade_p = matches.is_present("cascade");
+
+    let date_options = [
+        "%d %B %Y",
+        "%d-%B-%y"
+    ];
+
+    let date_col: Option<Vec<usize>> = match matches.value_of("datecol") {
+        Some(s) => { Some(parse_datecol(s)) }
+        None => { None }
+    };
 
     let mut last_line = CleanedRecord::new();
 
@@ -47,7 +56,7 @@ fn main() {
     for result in rdr.records() {
         match result {
             Ok(record) => {
-                process_line(record, &mut last_line, cascade_p);
+                process_line(record, &mut last_line, cascade_p, &date_col, &date_options);
                 writeln!(writer, "{}", json!(last_line)).unwrap();
             }
             Err(err) => {
@@ -58,13 +67,68 @@ fn main() {
     }
 }
 
-fn process_line(line: csv::StringRecord, last_line: &mut CleanedRecord, cascade_p: bool) {
+fn process_line(
+    line: csv::StringRecord,
+    last_line: &mut CleanedRecord,
+    cascade_p: bool,
+    date_col: &Option<Vec<usize>>,
+    date_options: &[&str])
+{
     for i in 0..(line.len()) {
         if let Some(field) = line.get(i) {
             if field.len() > 0 || ! cascade_p {
                 last_line[i].clear();
+
+                if let Some(col) = date_col {
+                    if col.contains(&i) {
+                        let mut iso_date = None;
+                        for format in date_options.iter() {
+                            match NaiveDate::parse_from_str(field, format) {
+                                Ok(date) => {
+                                    iso_date = Some(date);
+                                    break;
+                                }
+                                Err(_) => {}
+                            }
+                        }
+
+                        match iso_date {
+                            Some(date) => {
+                                last_line[i].push_str(&date.format("%Y-%m-%d")
+                                                           .to_string());
+                                continue;
+                            }
+                            None => {
+                                eprintln!("error converting date");
+                                process::exit(1);
+
+                            }
+                        }
+                    }
+                }
+
                 last_line[i].push_str(field);
             }
         }
     }
+}
+
+fn parse_datecol(raw: &str) -> Vec<usize> {
+    raw.split(",").map(|c|
+        match c.parse::<usize>() {
+            Ok(i) => {
+                if i < 1 {
+                    eprintln!("Expected a positive integer for --datecol: numbering starts at `1`.");
+                    process::exit(1);
+                }
+                else {
+                    i - 1
+                }
+            }
+            Err(_) => {
+                eprintln!("Couldn't not parse column number `{}` for --datecol: expected positive integer.", c);
+                process::exit(1);
+            }
+        }
+    ).collect()
 }
